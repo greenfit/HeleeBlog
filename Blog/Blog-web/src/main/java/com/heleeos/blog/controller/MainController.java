dc7575cc07d1b1504c8dade0743af67256679a68
Kiss <346886697@qq.com>
2017-03-09 17:37:11 +0800

    初始化导入

---
+++ Blog/Blog-web/src/main/java/com/heleeos/blog/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.heleeos.blog.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class MainController {
+    
+    /**
+     * 首页
+     */
+    @RequestMapping(value = { "/" , "index.html" })
+    public ModelAndView toIndex(HttpServletRequest request) {
+        ModelAndView modelAndView = new ModelAndView("index");
+        return modelAndView;
+    }
+}
---
+++ Blog/Blog-web/src/main/webapp/WEB-INF/pages/index.ftl
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h1>Heleeos - Blog</h1>
+</body>
+</html>
