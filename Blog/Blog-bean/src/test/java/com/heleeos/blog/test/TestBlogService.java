7ff786b482c4f6526577a3d3f6408358e39f6168
Kiss <346886697@qq.com>
2017-03-10 14:35:16 +0800

    [Add] bean and service test code

---
+++ Blog/Blog-bean/src/test/java/com/heleeos/blog/test/TestBlogService.java
@@ -0,0 +1,43 @@
+package com.heleeos.blog.test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.heleeos.blog.bean.Blog;
+import com.heleeos.blog.bean.BlogTag;
+import com.heleeos.blog.service.BlogService;
+import com.heleeos.blog.service.BlogTagService;
+
+public class TestBlogService extends TestConfig {
+    
+    @Autowired
+    private BlogService blogService;
+    
+    @Autowired
+    private BlogTagService blogTagService;
+    
+    @Test
+    public void add() {
+        String tags = "Spring,Spring 生命周期";
+        
+        Blog blog = new Blog();
+        blog.setTitle("Spring 生命周期");
+        blog.setTypeid(1);
+        blog.setTags(getTag(tags).toString());
+        blog.setContent("");
+        blog.setIstop(true);
+        blog.setManagerid(1);
+        
+        blogService.save(blog);
+    }
+    
+    private List<BlogTag> getTag(String tags) {
+        final List<BlogTag> lists = new LinkedList<>();
+        Arrays.asList(tags.split(",")).forEach(tag -> lists.add(blogTagService.get(tag)));
+        return lists;
+    }
+}
