9b9f38bd25d7b57cd5ba50885e47ff08ad186ae3
liyu <liyu26@meituan.com>
2017-10-13 14:55:43 +0800

    Fix

--- Blog/Blog-web-NodeJS/routes/hbs-helper.js
+++ Blog/Blog-web-NodeJS/routes/hbs-helper.js
@@ -11,9 +11,9 @@ hbs.registerHelper("datetime", function(timestamp) {
 
 hbs.registerHelper('larger', function(v1, v2, options) {
     if(v1 > v2) {
-	    return options.fn(this);
+        return options.fn(this);
     }
-	return options.inverse(this);
+    return options.inverse(this);
 });
 
 hbs.registerHelper("splitTags", function(tag) {
